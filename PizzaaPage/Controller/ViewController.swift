6460c8baa3b4ecd64aa271849dd780b7f1731649
Engin Bolat <engi.bolat@gmail.com>
2024-02-20 21:59:39 +0300

    first commit

---
+++ PizzaaPage/Controller/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  PizzaaPage
+//
+//  Created by Engin Bolat on 20.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title = "Pizza"
+        let appearance = UINavigationBarAppearance()
+        
+        appearance.backgroundColor = UIColor(named:"anaRenk")
+        appearance.titleTextAttributes = [
+            .foregroundColor: UIColor(named:"yaziRenk1")!,
+            .font:UIFont(name:"Pacifico-Regular",size:22)!
+        ]
+        
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance     = appearance
+    }
+}
+
